4a5dcd3c8b9368aad5e849a3531dd9332aaca2b6
KyuubiTila <tylerkatsumoto@gmail.com>
2024-05-22 01:16:52 +0100

    transparency for modal

--- components/GoalInput.tsx
+++ components/GoalInput.tsx
@@ -5,38 +5,50 @@ const GoalInput = ({
   goalInputHandler,
   addGoalHandler,
   enteredGoal,
+  toggleModal,
   isEditing,
   toggleModalCancel,
 }: {
   goalInputHandler: (inputText: string) => void;
   addGoalHandler: () => void;
   enteredGoal: string;
+  toggleModal: any;
   isEditing: boolean;
   toggleModalCancel: any;
 }) => {
   return (
-    <Modal animationType="slide" presentationStyle="overFullScreen">
-      <View className="items-center bg-yellow-500 flex-col justify-center min-h-[100vh] p-4">
-        <Image
-          className="h-32 w-32"
-          source={require("../assets/images/goal.png")}
-        />
-        <TextInput
-          className="border text-xl rounded-xl p-2 w-full"
-          placeholder="Your course goals"
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <View className="flex-row mt-4">
-          <View className="bg-blue-300 border rounded-xl mr-4">
-            <Button
-              color="black"
-              title={isEditing ? "Update Goal" : "Add Goal"}
-              onPress={addGoalHandler}
-            />
-          </View>
-          <View className="bg-red-400 border rounded-xl ml-4">
-            <Button color="black" title="Cancel" onPress={toggleModalCancel} />
+    <Modal
+      animationType="slide"
+      transparent={true}
+      presentationStyle="overFullScreen"
+    >
+      <View className="flex-1 justify-end">
+        <View className="bg-yellow-200 rounded-t-xl p-6">
+          <Image
+            className="h-32 w-32 self-center"
+            source={require("../assets/images/goal.png")}
+          />
+          <TextInput
+            className="border text-xl rounded-xl p-2 w-full my-4"
+            placeholder="Your course goals"
+            onChangeText={goalInputHandler}
+            value={enteredGoal}
+          />
+          <View className="flex-row mt-4">
+            <View className="bg-blue-300 border rounded-xl flex-1 mr-2">
+              <Button
+                color="black"
+                title={isEditing ? "Update Goal" : "Add Goal"}
+                onPress={addGoalHandler}
+              />
+            </View>
+            <View className="bg-red-400 border rounded-xl flex-1 ml-2">
+              <Button
+                color="black"
+                title="Cancel"
+                onPress={toggleModalCancel}
+              />
+            </View>
           </View>
         </View>
       </View>
